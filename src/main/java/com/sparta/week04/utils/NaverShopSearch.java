731a0779a92d25ac337d2c7bb23fabe237850c55
seungwook <rlatmd0829@naver.com>
2021-03-22 09:18:58 +0900

    Initial commit

---
+++ src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -0,0 +1,49 @@
+package com.sparta.week04.utils;
+
+import com.sparta.week04.models.ItemDto;
+import lombok.extern.apachecommons.CommonsLog;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class NaverShopSearch {
+    public String search(String query) {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", "NfoV3DzhPf5u0fdFk0mh");
+        headers.add("X-Naver-Client-Secret", "n3TjrKjvCP");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query="+query, HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value(); // 상태 코드 ex) 400
+        String response = responseEntity.getBody(); // 결과값 받아옴
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
+    }
+
+    public List<ItemDto> fromJSONtoItems(String result){
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items = rjson.getJSONArray("items");
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = (JSONObject) items.get(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            itemDtoList.add(itemDto);
+        }
+
+        return itemDtoList;
+    }
+
+}
